9090ce6a0f30afba276bca2af9e34a6b3b371b40
Deepak Chaurasia <Deepakchaurasia.work@gmail.com>
2018-06-06 09:26:30 +0530

    Add files via upload

---
+++ LargeMargin.py
@@ -0,0 +1,139 @@
+# https://github.com/pytorch/vision/blob/master/torchvision/models/densenet.py
+
+# https://github.com/felixgwu/img_classification_pk_pytorch/blob/master/models/densenet.py
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+#from torchvision.models.densenet import _Transition
+from lsoftmax import LSoftmaxLinear
+import math
+
+
+class _Transition(nn.Sequential):
+    def __init__(self,num_input_features,num_output_features):
+        super(_Transition,self).__init__()
+        self.add_module('norm',nn.BatchNorm2d(num_input_features))
+        self.add_module('relu',nn.ReLU(inplace=True))
+        self.add_module('conv',nn.Conv2d(num_input_features,num_output_features,kernel_size=1,stride=1,bias=False))
+        self.add_module('pool',nn.AvgPool2d(kernel_size=2,stride=2))
+
+
+
+
+class _DenseLayer(nn.Sequential):
+
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+
+        # If the bottle neck mode is set, apply feature reduction to limit the growth of features
+        # Why should we expand the number of features by bn_size*growth?
+
+        # https://stats.stackexchange.com/questions/194142/what-does-1x1-convolution-mean-in-a-neural-network
+        if bn_size > 0:
+            interChannels = 4*growth_rate
+            self.add_module('conv1', nn.Conv2d(
+                num_input_features, interChannels, kernel_size=1, stride=1, bias=False))
+            self.add_module('norm2', nn.BatchNorm2d(interChannels))
+            self.add_module('conv2', nn.Conv2d(
+                interChannels, growth_rate, kernel_size=3, padding=1, bias=False))
+        else:
+            self.add_module('conv2', nn.Conv2d(
+                num_input_features, growth_rate, kernel_size=3, stride=1, padding=1, bias=False))
+
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        new_features = super(_DenseLayer, self).forward(x)
+        if self.drop_rate > 0:
+            new_features = F.dropout(
+                new_features, p=self.drop_rate, training=self.training)
+        return torch.cat([x, new_features], 1)
+
+
+class _DenseBlock(nn.Sequential):
+
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(num_input_features+i *
+                                growth_rate, growth_rate, bn_size, drop_rate)
+            self.add_module('denselayer%d' % (i+1), layer)
+
+
+class DenseNet(nn.Module):
+    """Densenet-BC model class, based on
+    Args:
+        growth_rate (int) - how many filters to add each layer (`k` in paper)
+        block_config (list of 4 ints) - how many layers in each pooling block
+        num_init_features (int) - the number of filters to learn in the first convolution layer
+        bn_size (int) - multiplicative factor for number of bottle neck layers
+          (i.e. bn_size * k features in the bottleneck layer)
+        drop_rate (float) - dropout rate after each dense layer
+        num_classes (int) - number of classification classes
+    """
+
+    def __init__(self, growth_rate=4, block_config=(6, 6, 6), compression=0.5,
+                 num_init_features=24, bn_size=4, drop_rate=0, avgpool_size=8,
+                 num_classes=10,margin=2):
+        super(DenseNet, self).__init__()
+
+        # The first Convolution layer
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(1, num_init_features,
+                                kernel_size=3, stride=1, padding=1, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+        ]))
+        # Did not add the pooling layer to preserve dimension
+        # The number of layers in each Densnet is adjustable
+
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            Dense_block = _DenseBlock(num_layers=num_layers, num_input_features=num_features,
+                                      bn_size=bn_size, growth_rate=growth_rate, drop_rate=drop_rate)
+            # Add name to the Denseblock
+            self.features.add_module('denseblock%d' % (i + 1), Dense_block)
+
+            # Increase the number of features by the growth rate times the number
+            # of layers in each Denseblock
+            num_features += num_layers * growth_rate
+
+            # check whether the current block is the last block
+            # Add a transition layer to all Denseblocks except the last
+            if i != len(block_config):
+                # Reduce the number of output features in the transition layer
+
+                nOutChannels = int(math.floor(num_features*compression))
+
+                trans = _Transition(num_input_features=num_features,
+                                    num_output_features=nOutChannels)
+                self.features.add_module('transition%d' % (i + 1), trans)
+                # change the number of features for the next Dense block
+                num_features = nOutChannels
+
+            # Final batch norm
+            self.features.add_module('last_norm%d' % (i+1), nn.BatchNorm2d(num_features))
+        #self.last_norm = nn.BatchNorm2d(num_features)
+            # Linear layer
+            self.classifier = nn.Linear(num_features, num_features)
+            self.lsoftmax_linear = LSoftmaxLinear(input_dim=num_features,output_dim = num_classes,margin = margin)
+
+    def forward(self, x, target = None):
+        features = self.features(x)
+        #print(features)
+        out = F.relu(features,inplace=True)
+        out = F.avg_pool2d(out, kernel_size = 2).view(features.size(0), -1)
+        #out = self.classifier(out)
+        logit = self.lsoftmax_linear(input= out,target=target)
+        return logit
+        '''
+         conv_output = self.net(input)
+        batch_size = conv_output.size(0)
+        fc_input = conv_output.view(batch_size, -1)
+        fc_output = self.fc(fc_input)
+        logit = self.lsoftmax_linear(input=fc_output, target=target)
+        return logit
+        '''
---
+++ mnist+densnet.py
@@ -0,0 +1,199 @@
+
+# %matplotlib inline
+#from center_loss import CenterLoss 
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import datasets
+from sklearn.manifold import TSNE
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+# My model
+from LargeMargin import DenseNet as dn
+import torch.optim as optim
+from numpy import linalg
+import argparse
+
+# Hyper Parameters
+EPOCH = 5
+# train the training data n times, to save time, we just train 1 epoch
+BATCH_SIZE = 128
+LR = 0.001              # learning rate
+DOWNLOAD_MNIST = True   # set to False if you have downloaded
+
+#Normalize with the grayscale channel's mean and var
+#https://discuss.pytorch.org/t/normalization-in-the-mnist-example/457
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--margin',default=2,type=int)
+args = parser.parse_args()
+trainTransform = transforms.Compose([
+    transforms.RandomCrop(28, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+    
+testTransform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+
+
+# Mnist digits dataset
+train_data = torchvision.datasets.MNIST(
+    root='./mnist/',
+    train=True,                                     # this is training data
+    transform=trainTransform,    # Converts a PIL.Image or numpy.ndarray to
+                                                    # torch.FloatTensor of shape (C x H x W) and normalize in the range [0.0, 1.0]
+    download=DOWNLOAD_MNIST,                        # download it if you don't have it
+
+
+)
+
+# torchvision.datasets.MNIST(root='/home/ubuntu/notebooks/mnist', train=True)
+
+test_data = torchvision.datasets.MNIST(
+    root='./mnist/', 
+    train=False,
+    transform = testTransform)
+
+# plot one example
+print(train_data.train_data.size())                 # (60000, 28, 28)
+print(train_data.train_labels.size())               # (60000)
+
+# convert test data into Variable, pick 2000 samples to speed up testing
+# test_data = torchvision.datasets.MNIST(root='./mnist/', train=False,transform=testTransform)
+
+# Data Loader for easy mini-batch return in training, the image batch shape will be (50, 1, 28, 28)
+train_loader = DataLoader(
+    dataset=train_data, 
+    batch_size=BATCH_SIZE, 
+    shuffle=True)
+
+testLoader = DataLoader(
+        dataset=test_data,
+        batch_size=BATCH_SIZE, 
+        shuffle=False)
+
+# https://discuss.pytorch.org/t/feedback-on-pytorch-for-kaggle-competitions/2252/4
+
+model = dn(num_init_features=10,margin=args.margin)
+model.cuda()
+
+# Hyper Parameters
+EPOCH = 5
+
+# train the training data n times, to save time, we just train 1 epoch
+BATCH_SIZE = 128
+LR = 0.0001
+#optimizer = optim.SGD(model.parameters(), lr=LR,
+                            #momentum=0.9, weight_decay=1e-4)
+
+#center_loss = CenterLoss(num_classes=10, feat_dim=10, use_gpu=True)
+#params= list(model.parameters()) + list(center_loss.parameters())
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)   # optimize all cnn parameters
+loss_func = nn.CrossEntropyLoss()   # the target label is not one-hotted
+
+
+def accuracy():
+    test_loss = 0
+    correct = 0
+    i=0
+    ret = np.zeros((10000,10))
+    label = np.zeros(10000)
+    for data, target in testLoader:
+        data, target = Variable(data, volatile=True), Variable(target)
+        data = data.cuda()
+        target = target.cuda()
+        output = model(x=data,target=target)
+        test_loss += loss_func(output,target= target).data[0]
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+        ret[i:i+128,:] =output.cpu().detach().numpy() 
+        label[i:i+128] =pred 
+        i+=128
+
+    test_loss = test_loss
+    test_loss /= len(testLoader) # loss function already averages over batch size
+    print("Accuracy")
+    print(correct)
+    print("Loss")
+    print(test_loss)
+    return ret,label
+
+
+
+# training and testing
+for epoch in range(EPOCH):
+    for step, (x, y) in enumerate(train_loader):   # divide batch data, normalize x when iterate train_loader
+        b_x = Variable(x)   # batch x
+        b_y = Variable(y)   # batch y
+        #print(np.shape(b_x))
+        #print(np.shape(b_y))
+        output = model(x = b_x.cuda(),target =b_y.cuda())               # cnn output
+        #print(np.shape(b_x))
+        #print(np.shape(output))
+        #print(b_y.shape)
+        loss = loss_func(output.cuda(),b_y.cuda())
+        #print(loss)
+        optimizer.zero_grad()           # clear gradients for this training step
+        loss.backward()                 # backpropagation, compute gradients
+        optimizer.step()                # apply gradients
+
+       
+        if step % BATCH_SIZE == 0 :
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, step * len(b_x), len(train_loader.dataset),
+            100. * step / len(train_loader), loss.data[0]))
+    ret,label = accuracy()
+    print(ret.shape)
+ret=np.asarray(ret)
+label=np.asarray(label)
+np.savetxt('embed.tsv',ret,delimiter='\t')
+np.savetxt('metadata.tsv',label,delimiter='\t')
+embed = TSNE(n_components=2).fit_transform(ret)
+plt.scatter(embed[:,0],embed[:,1],c = label,s = 1, marker='x')
+plt.show()
+model.eval()
+
+
+
+
+def plot_kernels(tensor, num_cols=3):
+
+    num_kernels = tensor.shape[0]
+    num_rows = 1+ num_kernels // num_cols
+    fig = plt.figure(figsize=(num_cols,num_rows))
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+        ax1.imshow(tensor[i][0],cmap='gray')
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.show()
+
+mm= model.double()
+filters = mm.modules
+body_model = [i for i in mm.children()]
+body_model = body_model[0][3][3][4]
+layer1 = body_model
+tensor = layer1.weight.data.cpu().numpy()
+plot_kernels(tensor)
+
+#body_model = [i for i in mm.children()]
+
+#c = 0
+#for i in body_model[0]:
+ #   c+=1
+  #  print (i)
+
+#body_model[0][3][3][4]
+
